ef630c7821d434448a53c6fe35a861c9368e991d
Evgeniy Dashuk <evg.das@gmail.com>
2020-07-16 08:09:17 +0200

    add dataprovider for test

--- src/test/Test/CheckTextTest.java
+++ src/test/Test/CheckTextTest.java
@@ -1,5 +1,6 @@
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -8,34 +9,25 @@ import java.util.Map;
 import static org.testng.Assert.assertEquals;
 
 public class CheckTextTest {
-    private final Map<String, Boolean> parseExpr = new HashMap<String, Boolean>();
 
-    @BeforeClass
-    private void setUp() {
-        parseExpr.put("1+1", true);
-        parseExpr.put("1+10", true);
-        parseExpr.put("1*1", true);
-        parseExpr.put("1-1", true);
-        parseExpr.put("1**1", false);
-        parseExpr.put("X+X", true);
-        parseExpr.put("X+1", false);
-//        parseExpr.put("XX+1", true);
+    @DataProvider
+    public Object[][] parseExpr() {
+        return new Object[][]{
+                {"1+1", true},
+                {"1-1", true},
+                {"1*1", true},
+                {"1\1", false},
+                {"1++1", false}
 
-    }
-    @AfterTest
-    void tearDown() {
-        parseExpr.clear();
+
+        };
     }
 
-    @Test
-    public void isRightExpressionTest() {
-        for (Map.Entry<String, Boolean > entry : parseExpr.entrySet()) {
-            String exp = entry.getKey();
-            boolean actual=CheckText.isRightExpression(exp);
-            boolean expected = entry.getValue();
-            assertEquals(actual, expected);
-        }
 
+    @Test(dataProvider = "parseExpr")
+    public void isRightExpressionTest(String expr, boolean expected) {
+        boolean actual = CheckText.isRightExpression(expr);
+        assertEquals(actual, expected);
     }
 
     @Test
